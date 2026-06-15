df2fc10c9884cf0fab47733570146c1b33354e8a
kaie28 <bigor28baakapricho@gmail.com>
2026-06-15 12:09:13 +0900

    Update settings.py

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -25,6 +25,8 @@ ALLOWED_HOSTS = [
     os.environ.get('MY_IP', 'localhost'),
     '127.0.0.1',
 ]
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -58,12 +60,16 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+                'django.contrib.messages.context_processors.messages', 
+
+                ],  #context_processors の閉じカッコ
+        },  # oPTIONS の閉じカッコ
+    },  # 辞書データの閉じカッコ
 ]
 
+
+
+            
 WSGI_APPLICATION = 'todoproject.wsgi:application'
 
 DATABASES = {
@@ -72,12 +78,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db_data' / 'db.sqlite3',    
 　　　　　#↑～☆☆最新修正で「db_data」に追加する。(＝docker-compose.ymlファイルと連結)で、本番環境でも文字情報が消えない設定にする。
 　　　　　#↑　注意(※以前では「db.sqlite3」のみの設定にしてた。＝本番環境では文字消える/開発環境では消えないの設定だったので、危険なので上記に変更した)
+ 
     }
 }
 
-        
-    }
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
